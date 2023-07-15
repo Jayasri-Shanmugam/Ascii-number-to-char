5a296424f6704b87f5b9737b5716e2ae2fc4ed6d
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:44:01 +0530

    Add files via upload

---
+++ asciinumbertochar.java
@@ -0,0 +1,16 @@
+package Jayasrijavapractice;
+
+
+import java.util.Scanner;
+public class asciinumbertochar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Scanner sc=new Scanner(System.in);
+          int number=sc.nextInt();
+          char a=(char)number;
+          System.out.println(a);
+          sc.close();
+	}
+
+}
